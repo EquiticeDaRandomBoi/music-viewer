ad04fe06c36b364cfd1a5d0dc77bfc9909e6d883
AstroDaRandomBoi <evandoneghy@gmail.com>
2026-07-11 03:58:02 -0400

    feat: Task 2 - fabric.mod.json, mixin config, mod entrypoints

---
+++ mc-26.1.2/src/main/java/com/evand/musicplayer/MusicPlayerMod.java
@@ -0,0 +1,12 @@
+package com.evand.musicplayer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MusicPlayerMod implements ModInitializer {
+    public static final String MOD_ID = "musicplayer";
+
+    @Override
+    public void onInitialize() {
+        // Server-side: nothing to do
+    }
+}
